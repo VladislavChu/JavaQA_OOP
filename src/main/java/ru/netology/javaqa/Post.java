a99898cb26eff73c12a092c83b6c845cae376285
Vladislav Chudaev <117576235+VladislavChu@users.noreply.github.com>
2023-01-17 16:15:57 +0300

    Update Post.java

--- src/main/java/ru/netology/javaqa/Post.java
+++ src/main/java/ru/netology/javaqa/Post.java
@@ -1,11 +1,10 @@
 package ru.netology.javaqa;
 public class Post {
+    public FormDate birthday;
     public String name;
     public String surname;
     public String patronymic;
     public String passport;
     public String phone;
     public Boolean subscription;
-    public FormDate birthday;
-
 }
